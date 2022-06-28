dc27c90e21b5b25f96ab9e378f116181514e920a
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-28 15:26:07 -0700

    made adjustments to edit bill route

--- app/api/bill_routes.py
+++ app/api/bill_routes.py
@@ -39,20 +39,19 @@ def post_bill():
 
 @bill_routes.route('/<int:id>', methods=['PUT'])
 def edit_bill(id):
-    # bill = Bill.query.get(id)
-    # print(bill, "THIS IS BILL!!!!!!")
+    bill = Bill.query.get(id)
+    print("THIS IS BILL!!!!!!", bill)
     form = EditBillForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         data = form.data
         print("THIS IS THE DATA------", data)
-        edit_bill = Bill(id=data['id'],
-                         label=data['label'],
-                         amount=data['amount'],
-                         settled=data['settled'],
-                         created_at=datetime.now(),
-                         updated_at=datetime.now())
-        db.session.update(edit_bill)
+        bill.id=data['id'],
+        bill.label=data['label'],
+        bill.amount=data['amount'],
+        bill.settled=data['settled'],
+        bill.updated_at=datetime.now()
+
         db.session.commit()
         return edit_bill.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
