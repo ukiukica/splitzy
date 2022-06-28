8a6a1c60ad292e7ef7ae75148944058b53100bcd
uki <ukiworks034@gmail.com>
2022-06-28 09:33:52 -0400

    friended association table added

--- app/seeds/user_bills.py
+++ app/seeds/user_bills.py
@@ -3,51 +3,51 @@
 
 # def seed_user_bills():
 #     user_bill_1 = Bill(
-#         users=1, bills=1, amount= 62.50 )
+#         user_id=1, bill_id=1, user_amount= 62.50 )
 #     user_bill_2 = Bill(
-#         users=2, bills=1, amount= 62.50 )
+#         user_id=2, bill_id=1, user_amount= 62.50 )
 #     user_bill_3 = Bill(
-#         users=2, bills=2, amount= 1108.65 )
+#         user_id=2, bill_id=2, user_amount= 1108.65 )
 #     user_bill_4 = Bill(
-#         users=3, bills=2, amount= 1108.65 )
+#         user_id=3, bill_id=2, user_amount= 1108.65 )
 #     user_bill_5 = Bill(
-#         users=4, bills=2, amount= 1108.70 )
+#         user_id=4, bill_id=2, user_amount= 1108.70 )
 #     user_bill_6 = Bill(
-#         users=5, bills=3, amount= 42 )
+#         user_id=5, bill_id=3, user_amount= 42 )
 #     user_bill_7 = Bill(
-#         users=6, bills=3, amount= 42 )
+#         user_id=6, bill_id=3, user_amount= 42 )
 #     user_bill_8 = Bill(
-#         users=7, bills=4, amount= 184.33 )
+#         user_id=7, bill_id=4, user_amount= 184.33 )
 #     user_bill_9 = Bill(
-#         users=8, bills=4, amount= 184.33 )
+#         user_id=8, bill_id=4, user_amount= 184.33 )
 #     user_bill_10 = Bill(
-#         users=9, bills=4, amount= 184.34 )
+#         user_id=9, bill_id=4, user_amount= 184.34 )
 #     user_bill_11 = Bill(
-#         users=6, bills=5, amount= 445 )
+#         user_id=6, bill_id=5, user_amount= 445 )
 #     user_bill_12 = Bill(
-#         users=7, bills=5, amount= 445 )
+#         user_id=7, bill_id=5, user_amount= 445 )
 #     user_bill_13 = Bill(
-#         users=7, bills=5, amount= 445 )
+#         user_id=7, bill_id=5, user_amount= 445 )
 #     user_bill_14 = Bill(
-#         users=1, bills=6, amount= 10 )
+#         user_id=1, bill_id=6, user_amount= 10 )
 #     user_bill_15 = Bill(
-#         users=3, bills=6, amount= 10 )
+#         user_id=3, bill_id=6, user_amount= 10 )
 #     user_bill_16 = Bill(
-#         users=6, bills=7, amount= 1050 )
+#         user_id=6, bill_id=7, user_amount= 1050 )
 #     user_bill_17 = Bill(
-#         users=7, bills=7, amount= 1050 )
+#         user_id=7, bill_id=7, user_amount= 1050 )
 #     user_bill_18 = Bill(
-#         users=9, bills=8, amount= 160 )
+#         user_id=9, bill_id=8, user_amount= 160 )
 #     user_bill_19 = Bill(
-#         users=10, bills=8, amount= 160 )
+#         user_id=10, bill_id=8, user_amount= 160 )
 #     user_bill_20 = Bill(
-#         users=8, bills=9, amount= 260 )
+#         user_id=8, bill_id=9, user_amount= 260 )
 #     user_bill_21 = Bill(
-#         users=9, bills=9, amount= 260 )
+#         user_id=9, bill_id=9, user_amount= 260 )
 #     user_bill_22 = Bill(
-#         users=2, bills=10, amount= 46 )
+#         user_id=2, bill_id=10, user_amount= 46 )
 #     user_bill_23 = Bill(
-#         users=5, bills=10, amount= 46 )
+#         user_id=5, bill_id=10, user_amount= 46 )
 
 
 #     db.session.add(user_bill_1)
