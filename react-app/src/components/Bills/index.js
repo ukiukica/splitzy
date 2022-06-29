a255f967cc8226558ab3b28fcc4a8d6c9c5b7d60
uki <ukiworks034@gmail.com>
2022-06-29 12:57:38 -0400

    heroku deploy

--- react-app/src/components/Bills/index.js
+++ react-app/src/components/Bills/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { viewBills, removeBill } from "../../store/bills";
 import EditBillFormModal from "../EditBillModal";
