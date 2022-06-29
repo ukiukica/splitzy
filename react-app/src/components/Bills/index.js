52d0d77683f3b7532266a02c5ca0347ae048c8e1
olivia <oliviabir19@gmail.com>
2022-06-28 20:02:34 -0700

    add comment now modal

--- react-app/src/components/Bills/index.js
+++ react-app/src/components/Bills/index.js
@@ -5,6 +5,7 @@ import { viewBills, removeBill } from "../../store/bills";
 import EditBillFormModal from "../EditBillModal";
 import Comments from '../Comments';
 import CreateComment from '../CreateComment';
+import { Modal } from "../../context/Modal";
 
 function Bills() {
   const dispatch = useDispatch();
@@ -12,6 +13,8 @@ function Bills() {
   const bills = useSelector((state) => {
     return Object.values(state.bills);
   });
+
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     dispatch(viewBills());
@@ -31,9 +34,12 @@ function Bills() {
           </a>
           <EditBillFormModal billId={bill.id}/>
           <Comments billId={bill.id}/>
-          <button>
-            <CreateComment billId={bill.id}/>
-          </button>
+          <button onClick={() => setShowModal(true)}>Add a Comment</button>
+          {showModal && (
+            <Modal onClose={() => setShowModal(false)}>
+              <CreateComment billId={bill.id}/>
+            </Modal>
+          )}
           <br />
         </ul>
       ))}
