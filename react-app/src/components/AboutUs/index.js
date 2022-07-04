7b0980e389817d4d6ea56c83b61225b3a58cd203
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-07-03 18:20:27 -0700

    completed about us page

--- react-app/src/components/AboutUs/index.js
+++ react-app/src/components/AboutUs/index.js
@@ -39,6 +39,10 @@ function AboutUs() {
           <div className="creators-div">
             <div>
               <h3 className="creator-names">Uki</h3>
+              <div>
+              <img className="creator-photos" src="https://media-exp1.licdn.com/dms/image/C4D03AQEqpi1Y6t8NwA/profile-displayphoto-shrink_800_800/0/1595910271810?e=1662595200&v=beta&t=lwcYVIRIzKJpEMExeLlf7rUqCDgIKDsfym3yMBWzNW8"/>
+              <p>Software Engineer</p>
+              </div>
               <a href="https://www.linkedin.com/in/ukipavlovic/">
                 <img className="social-icons" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"/>
               </a>
@@ -48,6 +52,10 @@ function AboutUs() {
             </div>
             <div>
               <h3 className="creator-names">Caitlin</h3>
+              <div>
+              <img className="creator-photos" src="https://avatars.githubusercontent.com/u/93879557?s=400&u=53d6c9e706c8d9c3ce4ae64c760d4953e0c9631c&v=4"/>
+              <p>Software Engineer</p>
+              </div>
               <a href="https://www.linkedin.com/in/caitlin-buen-lucas/">
                 <img className="social-icons" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
               </a>
@@ -57,6 +65,10 @@ function AboutUs() {
             </div>
             <div>
               <h3 className="creator-names">Olivia</h3>
+              <div>
+              <img className="creator-photos" src="https://media.discordapp.net/attachments/988927733696442408/993310261270294578/Screenshot_20220703-171725_Facebook.jpg?width=1073&height=1055"/>
+              <p>Software Engineer</p>
+              </div>
               <a href="https://www.linkedin.com/in/olivia-bir-74b16b7b/">
                 <img className="social-icons" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
               </a>
@@ -66,6 +78,10 @@ function AboutUs() {
             </div>
             <div>
               <h3 className="creator-names">Danny</h3>
+              <div>
+              <img className="creator-photos" src="https://media-exp1.licdn.com/dms/image/C5603AQF9DV-JW5Jz3g/profile-displayphoto-shrink_800_800/0/1593757146453?e=1662595200&v=beta&t=MraukcTJA2L3A9iPwnImIOSSFC3pU5kvfsxFwzlFlo8"/>
+              <p>Software Engineer</p>
+              </div>
               <a href="https://www.linkedin.com/in/dannytoan/">
                 <img className="social-icons" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" />
               </a>
