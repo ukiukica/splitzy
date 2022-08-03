67202f2b7c88d5ec0ee3e5d70a021988b9a718ef
dannytoan <dannytoan1@gmail.com>
2022-08-03 14:20:57 -0700

    finished styling user overview modal

--- react-app/src/components/UserOverview/index.js
+++ react-app/src/components/UserOverview/index.js
@@ -5,32 +5,19 @@ import { useParams } from "react-router-dom";
 import "./useroverview.css";
 
 function UserOverview({ friend }) {
-  const { userId } = useParams();
-
   const [friends, setFriends] = useState([]);
   const [user, setUser] = useState([]);
 
   const sessionUser = useSelector((state) => state.session.user);
+  const users = useSelector((state) => state.users)
   const usersList = useSelector((state) => Object.values(state.users));
 
-  const friendId = usersList.filter((user) => user.username == friend);
-  console.log("USERSLIST", usersList);
-  console.log("ID", userId);
-  console.log("PARAMS", useParams());
-  console.log("FRIEND ID", friendId);
-  console.log("sessionUser", sessionUser);
-  // const friends = useSelector((state) => {
-  //   return Object.values(state.friends)
-  // })
+  const currentUser = usersList.filter((user) => user.username == friend);
 
-  // useEffect(() => {
-  //   dispatch(viewFriends(sessionUser.id))
-  // }, []);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(`/api/users/${friendId[0]?.id}`);
-      // const response = await fetch(`/api/users/${userId}`);
+      const response = await fetch(`/api/users/${currentUser[0]?.id}`);
       const responseData = await response.json();
       console.log("RESPONSE DATA", responseData);
       setUser(responseData);
@@ -65,84 +52,76 @@ function UserOverview({ friend }) {
     fetchData();
   };
 
+
+
+
+
+
   return (
     <div className="useroverview-container">
-      {/* <p className="friend-name">
-        Username: {user.username}
-        <br/>
-        Full Name: {user.first_name} {user.last_name}
-      </p> */}
+      <div id="useroverview-ctnr-one">
+
       <img
         id="profile-img-user-overview"
         src={`https://ui-avatars.com/api/?name=${user?.first_name}&rounded=true&background=random&uppercase=false&size=40`}
         alt="profile"
-      />
+        />
+        {friends.includes(user.username) ? (
+          <>
+            <a href={`/user-overview/${user.id}`}>
+              <button
+                onClick={(e) => {
+                  removeFriend(user.id);
+                }}
+                className="unfriend-btn"
+                >
+                Unfriend
+              </button>
+            </a>
+          </>
+        ) : (
+          <a href={`/user-overview/${user.id}`}>
+            <button
+              onClick={(e) => {
+                addFriend(user.id);
+              }}
+              className="submit-add-friend"
+              >
+              Add Friend
+            </button>
+          </a>
+        )}
+        </div>
       <div id="user-details-useroverview">
 
-      <p>Username: </p>
-      <p>{user.username}</p>
-      <p>
+      <p className="user-overview-label">Username: </p>
+      <p className="user-detail-useroverview">{user.username}</p>
+      <p className="user-overview-label">
         Full Name:
       </p>
-      <p>
+      <p className="user-detail-useroverview">
         {user.first_name} {user.last_name}
       </p>
-      <p>
+      <p className="user-overview-label">
         Email Address:
       </p>
-      <p>
+      <p className="user-detail-useroverview">
         {user.email}
       </p>
       </div>
 
       {/*~~~~~~~~~~~~~~~~~~ USER'S FRIENDS LIST ~~~~~~~~~~~~~~~~~~*/}
-      <div>
+
+      {/* <div id="useroverview-friends">
+
+      <div id="friends-section-useroverview">
         <p>Friends: </p>
-        {user?.friends?.map((friend) => (
+        {user?.friends?.length > 0 ? user?.friends?.map((friend) => (
           <div>{friend}</div>
-        ))}
+          )) : <div>None</div>}
       </div>
 
-      {/* ~~~~~~~~~~~~~~~~~~ USER'S MUTUAL LIST ~~~~~~~~~~~~~~~~~~*/}
-      {/* <div>
-          Mutual Friends:
-        </div>
-        {user?.friends?.map((friend) => {
-          let mutuals = sessionUser.friends.filter((mutual) => friend === mutual)
-          {mutuals.length ? mutuals.map((mutualFriend) => (
-            <div>{mutualFriend}</div>
-          )) : <div>You have no mutual friends</div>}
-          console.log("SESSION USER FRIENDS",sessionUser.friends)
-          console.log("FRIEND",friend)
-          console.log("MUTUALS", mutuals)
-        })} */}
-
-      {friends.includes(user.username) ? (
-        <>
-          {/* <p className="friends-label">(Friends)</p> */}
-          <a href={`/user-overview/${user.id}`}>
-            <button
-              onClick={(e) => {
-                removeFriend(user.id);
-              }}
-              className="unfriend-btn"
-            >
-              Unfriend
-            </button>
-          </a>
-        </>
-      ) : (
-        <a href={`/user-overview/${user.id}`}>
-          <button
-            onClick={(e) => {
-              addFriend(user.id);
-            }}
-            className="submit-add-friend"
-          >
-            Add Friend
-          </button>
-        </a>
-      )}
+      </div> */}
     </div>
   );
 }
